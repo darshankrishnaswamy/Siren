fedb27187339419fa256807ccaf4d19dd2437ec2
Alexander Sun <technoal9@gmail.com>
2019-10-26 19:36:24 -0400

    no axis plot

--- add.py
+++ add.py
@@ -8,8 +8,7 @@ import matplotlib.pyplot as plt
 direction = "./ECS/audio/44100"
 directory = Path(direction)
 audio, sampleRate = librosa.load(directory / '1-137-A-32.wav', sr=44100)
-fig, ax = plt.subplots()
-S, freqs, times, im = ax.specgram(audio, NFFT=4096, Fs=sampleRate,
+S, freqs, times= mlab.specgram(audio, NFFT=4096, Fs=sampleRate,
                                   window=mlab.window_hanning,
                                   noverlap=4096 // 2)
 S = S[::4, ::4]
