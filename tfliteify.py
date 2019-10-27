a5cab77976c5e70afefd01ae404cc729001d9adb
Arthur Zhang <azhangvo@gmail.com>
2019-10-26 21:52:14 -0400

    TFLITEIFY

---
+++ tfliteify.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+name = input("Name:\n")
+
+model = load_model("./models/" + str(name) + ".hdf5")
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+with open("./models/" + str(name) + ".tflite", "wb") as file:
+    file.write(tflite_model)
+
+import code
+
+variables = globals().copy()
+variables.update(locals())
+shell = code.InteractiveConsole(variables)
+shell.interact()
